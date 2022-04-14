50198c7ff60a6621805f385a826090639211dc3e
Dhan <duhan@polyv.net>
2022-04-14 18:07:46 +0800

    LSSDK测试

---
+++ Frameworks/MiniFramework/PLVLiveScenesSDK.framework/Headers/PLVLiveScenesSDK.h
@@ -0,0 +1,65 @@
+//
+//  PLVLiveScenesSDK.h
+//  PLVLiveScenesSDK
+//
+//  Created by zykhbl on 2018/8/2.
+//  Copyright © 2018年 PLV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PLVLiveScenesSDK.
+FOUNDATION_EXPORT double PLVLiveScenesSDKVersionNumber;
+
+//! Project version string for PLVLiveScenesSDK.
+FOUNDATION_EXPORT const unsigned char PLVLiveScenesSDKVersionString[];
+
+// 公共配置
+#import <PLVLiveScenesSDK/PLVLiveVideoConfig.h>
+
+#import <PLVLiveScenesSDK/PLVLiveDefine.h>
+
+#import <PLVLiveScenesSDK/PLVLiveConstants.h>
+
+#import <PLVLiveScenesSDK/PLVChannelInfoModel.h>
+#import <PLVLiveScenesSDK/PLVLiveVideoChannelMenuInfo.h>
+#import <PLVLiveScenesSDK/PLVPlaybackListModel.h>
+#import <PLVLiveScenesSDK/PLVCommodityModel.h>
+#import <PLVLiveScenesSDK/PLVLiveVClassAPI.h>
+#import <PLVLiveScenesSDK/PLVLiveVideoAPI.h>
+
+#import <PLVLiveScenesSDK/PLVSpeakMessage.h>
+#import <PLVLiveScenesSDK/PLVQuoteMessage.h>
+#import <PLVLiveScenesSDK/PLVImageMessage.h>
+#import <PLVLiveScenesSDK/PLVImageEmotionMessage.h>
+#import <PLVLiveScenesSDK/PLVCustomMessage.h>
+#import <PLVLiveScenesSDK/PLVRewardMessage.h>
+#import <PLVLiveScenesSDK/PLVChatroomManager.h>
+#import <PLVLiveScenesSDK/PLVLinkMicManager.h>
+#import <PLVLiveScenesSDK/PLVLinkMicGetTokenModel.h>
+#import <PLVLiveScenesSDK/PLVRTCStreamerManager.h>
+
+#import <PLVLiveScenesSDK/PLVPPTWebview.h>
+#import <PLVLiveScenesSDK/PLVWebViewBridge.h>
+#import <PLVLiveScenesSDK/PLVContainerWebViewBridge.h>
+
+#import <PLVLiveScenesSDK/PLVSocketManager.h>
+#import <PLVLiveScenesSDK/PLVSocketEventDefine.h>
+
+#import <PLVLiveScenesSDK/PLVInteractWebview.h>
+#import <PLVLiveScenesSDK/PLVInteractBaseApp.h>
+
+#import <PLVLiveScenesSDK/PLVPlayer.h>
+#import <PLVLiveScenesSDK/PLVLivePlayer.h>
+#import <PLVLiveScenesSDK/PLVLivePlaybackPlayer.h>
+
+#import <PLVLiveScenesSDK/PLVViewLogCustomParam.h>
+#import <PLVLiveScenesSDK/PLVWErrorManager.h>
+#import <PLVLiveScenesSDK/PLVWLogReporterManager.h>
+#import <PLVLiveScenesSDK/PLVConsoleLogger.h>
+
+#import <PLVLiveScenesSDK/PLVDocumentUploadClient.h>
+#import <PLVLiveScenesSDK/PLVDocumentUploadModel.h>
+
+#import <PLVLiveScenesSDK/PLVChannelClassManager.h>
+#import <PLVLiveScenesSDK/PLVHiClassManager.h>
---
+++ Frameworks/PLVLiveScenesSDK.framework/Headers/PLVLiveVideoConfig.h
@@ -0,0 +1,45 @@
+//
+//  PLVLiveVideoConfig.h
+//  PLVLiveScenesSDK
+//
+//  Created by ftao on 24/10/2017.
+//  Copyright © 2017 PLV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <PLVFoundationSDK/PLVConfig.h>
+
+/**
+ 直播 SDK 配置文件
+ */
+@interface PLVLiveVideoConfig : NSObject<PLVConfig>
+
+/// 是否已配置有效userId、appId、appSecret，默认为NO，有效指非空字符串
+@property (nonatomic, assign, readonly) BOOL configAccount;
+/// 可配置chatApi私有域名
+@property (nonatomic, strong, readonly) NSString *chatApiDomain;
+
+/// 弹出相册或相机时不能横竖屏切换
+@property (nonatomic, assign) BOOL unableRotate;
+/// 互动页面此时是否不能横竖屏切换
+@property (nonatomic, assign) BOOL triviaCardUnableRotate;
+/// 是否启用 HttpDNS，默认开启
+@property (nonatomic, assign) BOOL enableHttpDNS;
+/// 是否启用 IPV6，如果启用，将自动选择IP，取消HttpDNS
+@property (nonatomic, assign) BOOL enableIPV6;
+
++ (instancetype)sharedInstance;
+
+/// 配置账号信息
+/// PLV 后台，云直播->开发设置->身份认证：userId、AppID、AppSecret参数
+/// @param userId 用户ID
+/// @param appId 应用ID
+/// @param appSecret 应用密钥
+- (BOOL)configWithUserId:(NSString *)userId appId:(NSString *)appId appSecret:(NSString *)appSecret;
+
+/**
+ 私有域名配置
+ */
++ (void)setPrivateDomainWithData:(NSDictionary *)data;
+
+@end
